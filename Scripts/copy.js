2eeae59adc565b34d54c30789bd3aefcda161f45
Thasin Mahmud <thasinmahmud@gmail.com>
2022-06-25 13:37:27 +0600

    CDN Updated.

--- Scripts/copy.js
+++ Scripts/copy.js
@@ -8,3 +8,32 @@ function copyHTML() {
     note.classList.remove("note");
     }, 500)
 } 
+
+function copyJS() {
+    navigator.clipboard.writeText("https://raw.githack.com/Thasinmahmudbd/Cus.Cur/Thasin/Scripts/cusCur.js");
+
+    const note = document.querySelector('.noteTarget');
+
+    note.classList.add("note");
+    setTimeout(()=>{
+    note.classList.remove("note");
+    }, 500)
+}
+
+function copyCSS(curName) {
+
+    if(curName=='hulkSmash'){
+        navigator.clipboard.writeText("https://raw.githack.com/Thasinmahmudbd/Cus.Cur/Thasin/CursorStyle/hulkSmash.css");
+    }else if(curName=='captainAmerica'){
+        navigator.clipboard.writeText("https://raw.githack.com/Thasinmahmudbd/Cus.Cur/Thasin/CursorStyle/captainAmerica.css");
+    }else if(curName=='thorHammer'){
+        navigator.clipboard.writeText("https://raw.githack.com/Thasinmahmudbd/Cus.Cur/Thasin/CursorStyle/thorHammer.css");
+    }
+
+    const note = document.querySelector('.noteTarget');
+
+    note.classList.add("note");
+    setTimeout(()=>{
+    note.classList.remove("note");
+    }, 500)
+}
